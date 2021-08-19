03e79bc1d245da93edb1197af39ea11238ef3d52
Aswin Sankar P D <you@example.com>
2021-08-20 01:52:40 +0530

    task updation done

--- TodoApp/todos/views.py
+++ TodoApp/todos/views.py
@@ -5,13 +5,23 @@ from .forms import *
 
 def index(request):
 	data = Tasks.objects.all()
-	print(data)
-
 	form = TasksForm()
 	context={"data":data,"form":form}
 	if(request.method == "POST"):
 		form = TasksForm(request.POST)
 		if(form.is_valid()):
 			form.save()
-		return redirect('/todos/')
+		return redirect('/')
 	return render(request,'todos/list.html',context)
+
+
+def updateTask(request,pk):
+	task = Tasks.objects.get(id=pk)
+	form = TasksForm(instance=task)
+	if(request.method == "POST"):
+		form = TasksForm(request.POST,instance = task)
+		if(form.is_valid()):
+			form.save()
+		return redirect('/')
+	context = {"form":form}
+	return render(request,'todos/update.html',context)
