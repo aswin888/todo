09e2d32767f5531473cfca4e447dc8c4b92ac2e0
Aswin Sankar P D <you@example.com>
2021-08-20 00:02:43 +0530

    task creation done

--- TodoApp/todos/views.py
+++ TodoApp/todos/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .models import Tasks
+from .forms import *
 # Create your views here.
 
 def index(request):
-	return render(request,'todos/index.html')
+	data = Tasks.objects.all()
+	print(data)
+
+	form = TasksForm()
+	context={"data":data,"form":form}
+	if(request.method == "POST"):
+		form = TasksForm(request.POST)
+		if(form.is_valid()):
+			form.save()
+		return redirect('/todos/')
+	return render(request,'todos/list.html',context)
